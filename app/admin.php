800d6e3dcfb56f491bb9b55374effc2dbf9300d5
Andrey <staybox@gmail.com>
2018-03-12 17:13:29 +0300

    commit

--- app/admin.php
+++ app/admin.php
@@ -1,7 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Андрей
- * Date: 12.03.2018
- * Time: 16:10
- */
--- vp1.loft/index.php
+++ vp1.loft/index.php
@@ -1,4 +1,36 @@
 <?php
-if (strpos($_SERVER['REQUEST_URI'], "/") !== false) {
+/*if (strpos($_SERVER['REQUEST_URI'], "/") !== false) {
    require_once('../app/template.php');
+}*/
+
+// начинаем работать с сессией
+session_start();
+
+$appDir = realpath(__DIR__ . '/../app');
+
+// стартовая страница
+if ($_SERVER['REQUEST_URI'] == "/") {
+    require_once($appDir .DIRECTORY_SEPARATOR . 'template.php');
+    return 0;
 }
+
+// добавляем в базу
+if (!empty($_POST) && $_SERVER['REQUEST_URI'] == "/order/add") {
+    require_once($appDir .DIRECTORY_SEPARATOR . 'app.php');
+    return 0;
+}
+
+// просмотр пользователей (административная панель)
+if ($_SERVER['REQUEST_URI'] == "/admin/users") {
+    // тут код вывода
+    return 0;
+}
+
+// просмотр заказов (административная панель)
+if ($_SERVER['REQUEST_URI'] == "/admin/orders") {
+    // тут код вывода
+    return 0;
+}
+
+// такой страницы нет
+header("HTTP/1.0 404 Not Found");
